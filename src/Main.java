b2ba3c340085e7f9f9a179ab29a96f0a712493cd
Renata Scharf <Renata5207418@users.noreply.github.com>
2026-05-18 13:06:48 -0300

    Histórico de operações

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.util.Locale;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 
 
 public class Main{
@@ -10,6 +11,8 @@ public class Main{
         Scanner entrada = new Scanner(System.in);
         DecimalFormat formato = new DecimalFormat("0.##");
 
+        ArrayList<String> historico = new ArrayList<>();
+
         int opcao = -1;
 
         while (opcao != 0) {
@@ -18,9 +21,11 @@ public class Main{
             opcao = entrada.nextInt();
             
             if (opcao == 0) {
-                System.out.println("Encerrando a calculadora.");              
+                System.out.println("Encerrando a calculadora.");  
+            }else if (opcao == 5) {
+                exibirHistorico(historico);                        
             } else if (opcao < 1 || opcao > 4) {
-                System.out.println("Opção inválida. Por favor, escolha uma opção entre 1 e 4 ou 0 para sair.");
+                System.out.println("Opção inválida. Por favor, escolha uma opção entre 1 e 5 ou 0 para sair.");
             } else {
                 System.out.print("Digite o primeiro número: ");
                 double numero1 = entrada.nextDouble();
@@ -29,30 +34,38 @@ public class Main{
                 double numero2 = entrada.nextDouble();
 
                 double resultado;
+                String conta;
 
                 switch (opcao) {
                     case 1:
                         resultado = somar(numero1, numero2);
+                        conta = formato.format(numero1) + " + " + formato.format(numero2) + " = " +formato.format(resultado);
                         System.out.println("Resultado da soma: " + formato.format(resultado));
+                        historico.add(conta);
                         break;
                     case 2:
                         resultado = subtrair(numero1, numero2);
+                        conta = formato.format(numero1) + " - " + formato.format(numero2) + " = " + formato.format(resultado);
                         System.out.println("Resultado da subtração: " + formato.format(resultado));
+                        historico.add(conta);
                         break;
                     case 3:
                         resultado = multiplicar(numero1, numero2);
+                        conta = formato.format(numero1) + " * " + formato.format(numero2) + " = " + formato.format(resultado);
                         System.out.println("Resultado da multiplicação: " + formato.format(resultado));
+                        historico.add(conta);
                         break;
                     case 4:
                         if (numero2 == 0) {
                             System.out.println("Erro: Divisão por zero não é permitida.");
                         } else {
                             resultado = dividir(numero1, numero2);
-                            System.out.println("Resultado da divisão: " + formato.format(resultado));                     
+                            conta = formato.format(numero1) + " / " + formato.format(numero2) + " = " + formato.format(resultado);
+                            System.out.println("Resultado da divisão: " + formato.format(resultado));
+                            historico.add(conta);   
                         }
                         break;
             }
-
            
         }    
     }    
@@ -66,8 +79,21 @@ public class Main{
         System.out.println("2 - Subtração");
         System.out.println("3 - Multiplicação");
         System.out.println("4 - Divisão");
+        System.out.println("5 - Histórico de operações");
         System.out.println("Digite 0 para sair.");
         System.out.print("\nEscolha uma opção: ");
+    }
+
+    public static void exibirHistorico(ArrayList<String> historico) {
+        System.out.println("\n=== Histórico de Operações ===");
+        
+        if (historico.isEmpty()) {
+            System.out.println("Nenhuma operação realizada ainda.");
+        } else {
+            for (int i = 0; i < historico.size(); i++){
+                System.out.println((i + 1) + ") " + historico.get(i));
+            }            
+        }
     }
 
     public static double somar(double numero1, double numero2) {
