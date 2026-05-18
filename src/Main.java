ec078536f55995f19a2a81fbbe6c674dc7358384
Renata Scharf <Renata5207418@users.noreply.github.com>
2026-05-18 11:38:02 -0300

    Código organizado em métodos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.Locale;
+import java.text.DecimalFormat;
 
 
 public class Main{
@@ -7,65 +8,82 @@ public class Main{
 
         Locale.setDefault(Locale.US);
         Scanner entrada = new Scanner(System.in);
+        DecimalFormat formato = new DecimalFormat("0.##");
 
         int opcao = -1;
 
         while (opcao != 0) {
-            System.out.println("\n=== Calculadora ===");
-            System.out.println("1 - Soma");
-            System.out.println("2 - Subtração");
-            System.out.println("3 - Multiplicação");
-            System.out.println("4 - Divisão");
-            System.out.println("Digite 0 para sair");
-            System.out.println("\nEscolha uma opção:");
 
+            exibirMenu();
             opcao = entrada.nextInt();
-
+            
             if (opcao == 0) {
-                System.out.println("Encerrando a calculadora...");
+                System.out.println("Encerrando a calculadora.");              
             } else if (opcao < 1 || opcao > 4) {
-                System.out.println("Opção inválida, tente novamente.");
-            } else {       
-                
-                System.out.println("Digite o primeio número:");
+                System.out.println("Opção inválida. Por favor, escolha uma opção entre 1 e 4 ou 0 para sair.");
+            } else {
+                System.out.print("Digite o primeiro número: ");
                 double numero1 = entrada.nextDouble();
-
-                System.out.println("Digite o segundo número:");
+                
+                System.out.print("Digite o segundo número: ");
                 double numero2 = entrada.nextDouble();
 
                 double resultado;
 
                 switch (opcao) {
                     case 1:
-                        resultado = numero1 + numero2;
-                        System.out.println("resultado da soma: " + resultado);                               
-                        break; 
-
+                        resultado = somar(numero1, numero2);
+                        System.out.println("Resultado da soma: " + formato.format(resultado));
+                        break;
                     case 2:
-                        resultado = numero1 -numero2;
-                        System.out.println("Resultado da subtração: " + resultado);
+                        resultado = subtrair(numero1, numero2);
+                        System.out.println("Resultado da subtração: " + formato.format(resultado));
                         break;
-
                     case 3:
-                        resultado = numero1 * numero2;
-                        System.out.println("Resultado da multiplicação: " + resultado);
+                        resultado = multiplicar(numero1, numero2);
+                        System.out.println("Resultado da multiplicação: " + formato.format(resultado));
                         break;
-
                     case 4:
                         if (numero2 == 0) {
-                            System.out.println("Erro: não é possível dividir por zero.");
-                        
+                            System.out.println("Erro: Divisão por zero não é permitida.");
                         } else {
-                            resultado = numero1 / numero2;
-                            System.out.println("Resultado da divisão: " + resultado);
+                            resultado = dividir(numero1, numero2);
+                            System.out.println("Resultado da divisão: " + formato.format(resultado));                     
                         }
-                        break;        
-                }
-            }    
+                        break;
+            }
+
+           
         }    
+    }    
         entrada.close();
 
     }
 
+    public static void exibirMenu() {
+        System.out.println("\n=== Escolha a operação desejada ===");
+        System.out.println("\n1 - Soma");
+        System.out.println("2 - Subtração");
+        System.out.println("3 - Multiplicação");
+        System.out.println("4 - Divisão");
+        System.out.println("Digite 0 para sair.");
+        System.out.print("\nEscolha uma opção: ");
+    }
+
+    public static double somar(double numero1, double numero2) {
+        return numero1 + numero2;        
+    }
+    
+    public static double subtrair(double numero1, double numero2) {
+        return numero1 - numero2;        
+    }
+
+    public static double multiplicar(double numero1, double numero2) {
+        return numero1 * numero2;        
+    }   
+
+    public static double dividir(double numero1, double numero2) {
+        return numero1 / numero2;        
+    }
 
 }
